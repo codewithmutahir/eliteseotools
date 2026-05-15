24ab30624f5ec858e5d3c73d69cb9f20c291d8b7
codewithmutahir <mutharsoomro13@gmail.com>
2026-05-15 22:56:41 +0500

    Add Google Analytics tracking script

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,16 @@ export default function RootLayout({
               </div>
             </QueryProvider>
           </ThemeProvider>
+
+          <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZXPVDKBWM3"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-ZXPVDKBWM3');
+          </script>
         </body>
       </html>
     </ClerkProvider>
